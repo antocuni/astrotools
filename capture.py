9c5076a13f7347cbf09082c5035c5c113731f1d2
Antonio Cuni <anto.cuni@gmail.com>
2019-06-29 18:54:13 +0100

    move things to a class so that they are easier to customize

--- capture.py
+++ capture.py
@@ -11,28 +11,44 @@ def cmd(s):
     if ret != 0:
         raise ValueError(ret)
 
-def get_battery_level():
-    cmd("gphoto2 --get-config /main/status/batterylevel | grep -E '(Label|Current)'")
 
-def capture_many(n):
-    captured = 0
-    while True:
-        print
-        print 'capture %d/%d' % (captured+1, n)
-        try:
-            cmd('gphoto2 --capture-image | cat')
-        except ValueError:
-            print 'Error when executing gphoto2, sleeping some seconds before retrying'
-            time.sleep(5)
-            print 'Retry!'
-        else:
-            captured += 1
+class Camera(object):
 
-        if captured == n:
-            break
+    def __init__(self):
+        print "Setting capturetarget to SD"
+        cmd('gphoto2 --set-config capturetarget=1 | cat')
 
-        if captured % 10 == 0:
-            get_battery_level()
+    def capture_many(self, n):
+        print "Setting drivemode to single"
+        cmd('gphoto2 --set-config /main/capturesettings/drivemode=0 | cat')
+        self._do_n_captures(n)
+
+    def get_battery_level(self):
+        cmd("gphoto2 --get-config /main/status/batterylevel | grep -E '(Label|Current)'")
+
+    def _do_one_capture(self):
+        cmd('gphoto2 --capture-image | cat')
+
+    def _do_n_captures(self, n):
+        captured = 0
+        while True:
+            print
+            print 'capture %d/%d' % (captured+1, n)
+            try:
+                self._do_one_capture()
+            except ValueError:
+                print ('Error when executing gphoto2, '
+                       'sleeping some seconds before retrying')
+                time.sleep(5)
+                print 'Retry!'
+            else:
+                captured += 1
+
+            if captured == n:
+                break
+
+            if captured % 10 == 0:
+                self.get_battery_level()
 
 def main():
     # TODO: I saw this output:
@@ -52,11 +68,9 @@ def main():
     # It means that capture 102 was NOT done, but the retvalue was still 0. We
     # need a better way to check for errors
 
-    NUM = int(sys.argv[1])
-    cmd('gphoto2 --set-config /main/capturesettings/drivemode=0') # 0 ==> single
-    cmd('gphoto2 --set-config capturetarget=1')
-    capture_many(NUM)
-
+    n = int(sys.argv[1])
+    cam = Camera()
+    cam.capture_many(n)
     print
     print
     print '===================='
