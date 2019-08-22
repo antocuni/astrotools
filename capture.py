a4626f6a1baaef247367a435591e68324d53522c
Antonio Cuni <anto.cuni@gmail.com>
2019-08-22 22:18:44 +0100

    add support for the Nikon

--- capture.py
+++ capture.py
@@ -4,6 +4,7 @@
 
 import sys
 import os
+import commands
 import time
 
 def cmd(s):
@@ -14,30 +15,52 @@ def cmd(s):
 def set_config(x):
     cmd('gphoto2 --set-config %s | cat' % x)
 
-class Camera(object):
+class BaseCamera(object):
 
-    def __init__(self):
+    def __init__(self, model):
+        print "Camera is:", model
         print "Setting capturetarget to SD"
         set_config("capturetarget=1")
-        print "Setting drivemode to single"
-        set_config('/main/capturesettings/drivemode=0')
+        self.set_drivemode()
+
+    @staticmethod
+    def autodetect():
+        def error(output):
+            print 'cannot auto-detect the camera:'
+            print output
+            raise ValueError()
+        #
+        output = commands.getoutput('gphoto2 --auto-detect')
+        lines = output.splitlines()
+        if len(lines) == 2:
+            return None
+        elif len(lines) != 3:
+            error()
+        #
+        model = lines[-1].lower()
+        if model.startswith('canon'):
+            return CanonCamera(model)
+        elif model.startswith('nikon'):
+            return NikonCamera(model)
+        else:
+            error()
+
+    def set_drivemode(self):
+        raise NotImplementedError
+
+    def set_shutterspeed_to_bulb(self):
+        raise NotImplementedError
+
+    def do_one_bulb(self):
+        raise NotImplementedError
 
     def capture_many(self, n, shutterspeed=None):
         if shutterspeed is not None:
-            print "Setting shutterspeed to bulb"
-            set_config('/main/capturesettings/shutterspeed=bulb')
+            self.set_shutterspeed_to_bulb()
         self._do_many_captures(n, shutterspeed)
 
     def get_battery_level(self):
         cmd("gphoto2 --get-config /main/status/batterylevel | grep -E '(Label|Current)'")
-
-    def _do_one_bulb(self, shutterspeed):
-        cmd("gphoto2 "
-            "--set-config eosremoterelease=Immediate "
-            "--wait-event=%ss "
-            "--set-config eosremoterelease='Release Full' "
-            "--wait-event=2s "
-            " | grep -v UNKNOWN " % shutterspeed)
 
     def _do_many_captures(self, n, shutterspeed):
         captured = 0
@@ -48,7 +71,7 @@ class Camera(object):
                 if shutterspeed is None:
                     cmd('gphoto2 --capture-image | cat')
                 else:
-                    self._do_one_bulb(shutterspeed)
+                    self.do_one_bulb(shutterspeed)
                     time.sleep(5)
             except ValueError:
                 print ('Error when executing gphoto2, '
@@ -63,6 +86,52 @@ class Camera(object):
 
             if captured % 10 == 0:
                 self.get_battery_level()
+
+
+class CanonCamera(BaseCamera):
+    """
+    Tested with Canon EOS M50
+    """
+
+    def set_drivemode(self):
+        print "Setting drivemode to single"
+        set_config('/main/capturesettings/drivemode=0')
+
+    def set_shutterspeed_to_bulb(self):
+        print "Setting shutterspeed to bulb"
+        set_config('/main/capturesettings/shutterspeed=bulb')
+
+    def do_one_bulb(self, shutterspeed):
+        cmd("gphoto2 "
+            "--set-config eosremoterelease=Immediate "
+            "--wait-event=%ss "
+            "--set-config eosremoterelease='Release Full' "
+            "--wait-event=2s "
+            " | grep -v UNKNOWN " % shutterspeed)
+
+class NikonCamera(BaseCamera):
+    """
+    Tested with Nikon D5300
+    """
+
+    def set_drivemode(self):
+        # XXX implement me eventually
+        pass
+
+    def set_shutterspeed_to_bulb(self):
+        # XXX investigate Bulb vs Time
+        print "Setting shutterspeed to bulb"
+        set_config('/main/capturesettings/shutterspeed=Bulb')
+
+    def do_one_bulb(self, shutterspeed):
+        # apparently, with the nikon DSC5300 it is enough to set bulb=1 to start
+        # the capture. Then the camure will stop the capture automatically when
+        # gphoto2 disconnects
+        cmd("gphoto2 "
+            "--set-config /main/actions/bulb=1 "
+            "--wait-event=%ss "
+            " | grep -v UNKNOWN " % shutterspeed)
+
 
 def main():
     # TODO: I saw this output:
@@ -82,7 +151,7 @@ def main():
     # It means that capture 102 was NOT done, but the retvalue was still 0. We
     # need a better way to check for errors
 
-    # 
+    #
     if len(sys.argv) == 3:
         n = int(sys.argv[1])
         shutterspeed = int(sys.argv[2])
@@ -93,7 +162,10 @@ def main():
         print "Usage: ./capture.py N [shutterspeed]"
         sys.exit(1)
 
-    cam = Camera()
+    cam = BaseCamera.autodetect()
+    if cam is None:
+        print 'No camera found'
+        return
     cam.capture_many(n, shutterspeed)
     print
     print
@@ -103,4 +175,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
