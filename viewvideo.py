1a9540418d6a74dd344313c8a28468444caed300
Antonio Cuni <anto.cuni@gmail.com>
2020-01-30 17:34:35 +0100

    use a trackbar to select the frame

--- viewvideo.py
+++ viewvideo.py
@@ -38,32 +38,28 @@ class VideoFile(object):
 class MyViewer(object):
 
     def __init__(self, fname):
+        self.fname = fname
+        self.title = fname
         self.cap = VideoFile(fname)
+        cv2.namedWindow(self.title)
+        cv2.createTrackbar('Frame', self.title , 0, self.cap.get_frame_count(), self.on_frame)
 
     def run(self):
         fps = 25.0 # seconds
         ms_delay = int(1000/fps) # milliseconds per frame
         with self.cap:
-            i = 0
-            self.show(i)
+            self.on_frame(0)
             while True:
                 ch = chr(cv2.waitKey(ms_delay) & 0xFF)
                 if ch == 'q':
                     break
-                elif ch == '+':
-                    i += 1
-                    self.show(i)
-                elif ch == '-':
-                    i -= 1
-                    self.show(i)
 
-
-    def show(self, n):
-        print('Frame', n)
+    def on_frame(self, n):
         frame = self.cap.get_frame(n)
         assert frame is not None
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
+        cv2.imshow(self.title, gray)
+
 
 
 
